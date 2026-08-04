526fd84295103fbf2b0e9f2c7b21fe597cea6acd
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-04 14:44:42 -0400

    Created Task 7 Six-bar linkage
    Created Task 7 coding document

---
+++ Task_7_Six_Bar_Linkage.m
@@ -0,0 +1,17 @@
+% Static analysis of linkage with MATLAB code
+
+clr
+clear
+
+% Define coordinates
+A = [7 4 0];
+B = [5 16 0];
+C = [25 25 0];
+D = [23 10 0];
+E = [18 35 0];
+F = [43 32 0];
+G = [45 17 0];
+
+% Define center of mass
+S1 = (A+B)/2;
+S2 = (B+C+E)/2;
