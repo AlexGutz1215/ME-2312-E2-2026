a2f698d0f9721df1c74993bac5acf2a1a6874765
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-05 21:03:58 -0400

    Update4 Task 9 Circle Intersection
    Added plotting B function, as well as all C related functions

--- Task_9_Circle_Intersection.m
+++ Task_9_Circle_Intersection.m
@@ -28,7 +28,71 @@ if (initial_angle_AB < 0)
     initial_angle_AB = initial_angle_AB + (2 * pi);
 end
 
+% Storing values of B
+numSteps = 360;
+new_B_joint_x = zeros(1, numSteps + 1);
+new_B_joint_y = zeros(1, numSteps + 1);
+
+new_B_joint_x(1) = B(1);
+new_B_joint_y(1) = B(2);
+
+% Storing values of C
+new_C_joint_x = zeros(1, numSteps + 1);
+new_C_joint_y = zeros(1, numSteps + 1);
+
+new_C_joint_x(1) = C(1);
+new_C_joint_y(1) = C(2);
+
+
 % For-loop
 for theta = 1:1:360
     B_new = A + [AB * cos(initial_angle_AB + deg2rad(theta)) AB * sin(initial_angle_AB + deg2rad(theta)) 0];
+
+    new_B_joint_x(theta + 1) = B_new(1);
+    new_B_joint_y(theta + 1) = B_new(2);
+
+    % Compute the intersections and obtain new joint C
+    % with D as center, CD as radius, need another circle
+
+    % Compute the intersection points of the two circles
+    syms x y
+    eq1 = (x -B_new(1))^2 + (y - B_new(2))^2 == BC^2;
+    eq2 = (x - D(1))^2 + (y - D(2))^2 == CD^2;
+
+    % Solve the system of equations
+    intersectionPoints = solve([eq1, eq2], [x, y]);
+
+    % Assumption is that there will be a point generated
+    C_new_1 = double([intersectionPoints.x(1), intersectionPoints.y(1), 0]); % Take the first intersection
+    C_new_2 = double([intersectionPoints.x(2), intersectionPoints.y(2), 0]);
+
+    % Compare new Cs with the previous C value
+    % Compare new C positions with the original C value
+    dist_C1 = norm(C_new_1 -C);
+    dist_C2 = norm(C_new_2 -C);
+
+    % Whichever distance is the smallest, that C will be the new_C
+    if dist_C1 < dist_C2
+        C_new = C_new_1;
+    else
+        C_new = C_new_2;
+    end
+
+    new_C_joint_x(theta + 1) = C_new(1);
+    new_C_joint_y(theta + 1) = C_new(2);
+
+    % Another method of solving:
+    % circcirc function
 end
+
+% Plot the trajectory of point B
+figure;
+plot(new_B_joint_x, new_B_joint_y, 'b-', 'LineWidth', 2);
+hold on;
+plot(A(1), A(2), 'ro', 'MarkerSize', 8, 'DisplayName', 'Point A');
+plot(B(1), B(2), 'go', 'MarkerSize', 8, 'DisplayName', 'Initial Point B');
+xlabel('X Coordinate');
+ylabel('Y Coordinate');
+title('Trajectory of Point B');
+legend show;
+grid on;
