0cf779534fdafa0c3c4aaf5b7945ffdc0051380b
LadderMcLadder <adgutierrez0311@gmail.com>
2026-08-06 21:09:19 -0400

    Created multiple files
    Created the following files:
    function_kinematics_importfile
    import_data_spreadsheet_Task_10
    Task_16_Physical_Modeling

---
+++ Task_16_Physical_Modeling.m
@@ -0,0 +1,46 @@
+% Differential Equation Cr Bumper Problem
+% Mx'' + Dx' + Kx = 0
+% New initial condition
+
+% define parameters
+M = 8; % Mass - kg
+D = 50; % Damping coefficient - Ns/m
+K = 100; % Spring constant - N/m
+
+syms x(t) % x(t) is the x in Mx'' + Dx' + Kx = 0
+
+eqn1 = M * diff(x, t, 2) + D*diff(x, t) + K*x == 0;
+% Note: in the above equation, x'' is written as diff(x, t, 2)
+
+% x' is diff(x, t)
+Dx = diff(x, t);
+
+% Specifying Inital conditions
+intialCon = [x(0) == 0, Dx(0) == 10];
+
+% Solving for X(t)
+solutionX = dsolve(eqn1, intialCon);
+
+% Solving for V(t) - Velocity as function of time
+solutionV = diff(solutionX);
+
+% Solving for A(t) - Acceleration as function of time
+solutionA = diff(solutionV);
+
+% Plot symbolic solutions with fplot
+figure('Color', 'w', 'Position', [200 200 900 600]);
+
+subplot(2, 2, 1);
+fplot(solutionX, [0 10], 'LineWidth', 1.5)
+xlabel('Time (s)'); ylabel('Displacement (m)');
+title('Displacement x(t)'); grid on;
+
+subplot(2, 2, 2);
+fplot(solutionV, [0 10], 'LineWidth', 1.5)
+xlabel('Time (s)'); ylabel('Velocity (m/s)');
+title('Velocity x''(t)'); grid on;
+
+subplot(2, 2, 3);
+fplot(solutionA, [0 10], 'LineWidth', 1.5)
+xlabel('Time (s)'); ylabel('Acceleration (m/s^2)');
+title('Velocity x''(t)'); grid on;
